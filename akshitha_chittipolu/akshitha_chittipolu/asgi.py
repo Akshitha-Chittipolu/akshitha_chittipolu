e385556d108c3e44a67ddd3d07ba11b2fb51eeaa
Akshitha Chittipolu <chittipoluakshitha2023@gmail.com>
2023-12-11 16:23:18 -0800

    first commit

---
+++ akshitha_chittipolu/akshitha_chittipolu/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for akshitha_chittipolu project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'akshitha_chittipolu.settings')
+
+application = get_asgi_application()
